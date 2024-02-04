d2b0da5379d252d115d2448a932666247ad8e910
Rachit Garg <rachitgarg56@gmail.com>
2024-02-04 22:53:40 +0530

    code updated

--- script.js
+++ script.js
@@ -74,3 +74,51 @@ function animation(keyPressed) {
         }, 100)
     }
 };
+
+
+
+function handleClick(e) {
+    switch (e.target.innerText) {
+        case 'w': {
+            const audio1 = new Audio('./audios/tom-1.mp3');
+            audio1.play();
+            break;
+        }
+        case 'a': {
+            const audio2 = new Audio('./audios/tom-2.mp3');
+            audio2.play();
+            break;
+        }
+        case 's': {
+            const audio3 = new Audio('./audios/tom-3.mp3');
+            audio3.play();
+            break;
+        }
+        case 'd': {
+            const audio4 = new Audio('./audios/tom-4.mp3');
+            audio4.play();
+            break;
+        }
+        case 'j': {
+            const audio5 = new Audio('./audios/snare.mp3');
+            audio5.play();
+            break;
+        }
+        case 'k': {
+            const audio6 = new Audio('./audios/kick-bass.mp3');
+            audio6.play();
+            break;
+        }
+        case 'l': {
+            const audio7 = new Audio('./audios/crash.mp3');
+            audio7.play();
+            break;
+        }
+    }
+
+    key.innerText = e.key;
+    keyCode.innerText = e.keyCode;
+
+    animation(e.key);
+}
+
